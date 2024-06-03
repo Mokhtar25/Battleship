cbca2352816a36e14aaefa6f2683621a579a065f
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-03 21:59:10 +0200

    working on the backend

---
+++ src/frontend/script.js
@@ -0,0 +1,23 @@
+const grid = document.querySelector(".grid");
+
+const frag = create();
+
+grid.append(frag);
+
+function create() {
+  const frag = document.createDocumentFragment();
+
+  for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
+      const tmp = document.createElement("div");
+      tmp.classList.add("item");
+
+      tmp.value = [i, i];
+      tmp.addEventListener("click", () => {
+        console.log(tmp.value);
+      });
+      frag.append(tmp);
+    }
+  }
+  return frag;
+}
