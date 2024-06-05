403db0f8dfa4c0db00d030a887109aca76384019
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-05 15:26:18 +0200

    making the front end and adding lines into the squares

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -2,21 +2,22 @@ const grid = document.querySelector(".grid1");
 
 const grid2 = document.querySelector(".grid2");
 
-const frag = create();
+const frag = create(1);
 
-const frag2 = create();
+const frag2 = create(2);
 grid2.append(frag2);
 grid.append(frag);
 
-function create() {
+function create(type) {
   const frag = document.createDocumentFragment();
 
   for (let i = 0; i < 10; i++) {
     for (let i = 0; i < 10; i++) {
       const tmp = document.createElement("div");
       tmp.classList.add("item");
+      tmp.value = { move: [i, i], type: type };
+      tmp.textContent = "~";
 
-      tmp.value = [i, i];
       tmp.addEventListener("click", () => {
         console.log(tmp.value);
       });
