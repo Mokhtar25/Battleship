fd69ba36955d8341478918568e4bce7583b81577
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-05 18:33:31 +0200

    binding the back to front end

--- dist/game.js
+++ dist/game.js
@@ -1,10 +1,8 @@
 import MakePlayer from "./player.js";
-import PromptSync from "prompt-sync";
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-const prompt = PromptSync();
-function GameLogic() {
+export default function GameLogic() {
     const player1 = MakePlayer("human", 1);
     const cmp = MakePlayer("cmp", 2);
     player1.PlaceDefualt();
@@ -12,15 +10,10 @@ function GameLogic() {
     let turn = 1;
     while (!cmp.GetBoard().GameOver() && !player1.GetBoard().GameOver()) {
         if (turn === 1) {
-            console.table(cmp.GetBoard().getBoard());
-            const attack = prompt("chose a place");
-            let tmp = attack.split("");
-            let list = [+tmp[0], +tmp[2]];
-            cmp.RecaiveAttack(list);
+            const board = cmp.GetBoard().getBoard();
             turn = 2;
         }
         else {
-            console.table(player1.GetBoard().getBoard());
             let list = [getRandomInt(9), getRandomInt(9)];
             player1.RecaiveAttack(list);
             turn = 1;
@@ -28,4 +21,3 @@ function GameLogic() {
     }
     console.log("done");
 }
-GameLogic();
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,7 @@
-const grid: HTMLInputElement | null = document.querySelector(".grid1");
-const grid2: HTMLInputElement | null = document.querySelector(".grid2");
+import GameLogic from "./game.js";
+import MakePlayer from "./player.js";
+const grid: HTMLElement | null = document.querySelector(".grid1");
+const grid2: HTMLElement | null = document.querySelector(".grid2");
 const frag = create(1);
 const frag2 = create(2);
 
@@ -7,6 +9,62 @@ if (grid && grid2) {
   grid2.append(frag2);
   grid.append(frag);
 }
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+function GameLogicDom() {
+  const player1 = MakePlayer("human", 1);
+  const cmp = MakePlayer("cmp", 2);
+  player1.PlaceDefualt();
+  cmp.PlaceDefualt();
+
+  let turn = 1;
+
+  const play = (attack: number[], type: number) => {
+    if (type === 1 && turn === 1) {
+      const board = cmp.GetBoard().getBoard();
+      const childern: HTMLCollection | undefined = grid?.children;
+      if (!childern) return;
+      UpdateBoard(board, childern);
+
+      cmp.RecaiveAttack(attack);
+
+      turn = 2;
+    } else if (type === 2 && turn === 2) {
+      const board = player1.GetBoard().getBoard();
+      const childern: HTMLCollection | undefined = grid2?.children;
+      if (!childern) return;
+      UpdateBoard(board, childern);
+      let list = [getRandomInt(9), getRandomInt(9)];
+      player1.RecaiveAttack(list);
+      turn = 1;
+    }
+    if (cmp.GetBoard().GameOver() || player1.GetBoard().GameOver()) {
+      if (grid) grid.textContent = "win";
+    }
+  };
+
+  return { play, player1, cmp };
+}
+
+function UpdateBoard(borad: number[][], frag: HTMLCollection) {
+  let items: number[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      items.push(borad[i][j]);
+    }
+  }
+
+  for (const item of frag) {
+    item.textContent = `${items[0]}`;
+    items.shift();
+  }
+}
+const game = GameLogicDom();
+
 function create(type: number): DocumentFragment {
   const frag = document.createDocumentFragment();
 
@@ -19,11 +77,10 @@ function create(type: number): DocumentFragment {
 
       tmp.addEventListener("click", () => {
         const data: string | null = tmp.getAttribute("data-value");
-        console.log(data);
         if (data === null) return;
         const move = [+data[0], +data[1]];
-        const type = data[2];
-        console.log(move, type);
+        const type = +data[2];
+        game.play(move, type);
       });
       frag.append(tmp);
     }
